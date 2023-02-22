8130fdcab58c3850c40f4ae07a92eab79ee1260c
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-02-22 14:44:54 +0400

    string interpolation

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+//using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product?[] products = Product.GetProducts();
+
+            return View(new string[] { $"Name: {products[0]?.Name}, Price: {products[0]?.Price}" });
+        }
+    }
+}
