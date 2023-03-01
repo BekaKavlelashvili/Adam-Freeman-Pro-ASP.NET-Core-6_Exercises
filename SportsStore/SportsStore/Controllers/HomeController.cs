3921758246422a48e5569c05378404b17de79263
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-03-01 15:17:09 +0400

    added getting of products with pagination

--- SportsStore/SportsStore/Controllers/HomeController.cs
+++ SportsStore/SportsStore/Controllers/HomeController.cs
@@ -1,9 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() => View();
+        private IStoreRepository _repository;
+        public int PageSize = 4;
+
+        public HomeController(IStoreRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ViewResult Index(int productPage = 1) => View(new ProductsListViewModel
+        {
+            Products = _repository.Products
+            .OrderBy(x => x.ProductId)
+            .Skip((productPage - 1) * PageSize)
+            .Take(PageSize),
+            PagingInfo = new PagingInfo()
+            {
+                CurrentPage = productPage,
+                ItemsPerPage = PageSize,
+                TotalItems = _repository.Products.Count()
+            }
+        });
     }
 }
--- SportsStore/SportsStore/Program.cs
+++ SportsStore/SportsStore/Program.cs
@@ -16,6 +16,9 @@ var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
 app.UseStaticFiles();
+app.MapControllerRoute("pagination",
+    "Products/Page{ProductPage}",
+    new { Controller = "Home", action = "Index" });
 app.MapDefaultControllerRoute();
 
 SeedData.EnsurePopulated(app);
