fec092fff9038da6e5026128a90d008ffa0282dc
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-03-06 22:28:58 +0400

    added remove items and  checkout cart

--- SportsStore/SportsStore/Models/Cart.cs
+++ SportsStore/SportsStore/Models/Cart.cs
@@ -4,7 +4,7 @@
     {
         public List<CartLine> Lines { get; set; } = new List<CartLine>();
 
-        public void AddItem(Product product, int quantity)
+        public virtual void AddItem(Product product, int quantity)
         {
             CartLine? line = Lines.Where(x => x.Product.ProductId == product.ProductId).FirstOrDefault();
 
@@ -22,13 +22,13 @@
             }
         }
 
-        public void RemoveLine(Product product) =>
+        public virtual void RemoveLine(Product product) =>
             Lines.RemoveAll(x => x.Product.ProductId == product.ProductId);
 
         public decimal ComputeTotalValue() =>
              Lines.Sum(x => x.Quantity * x.Product.Price);
 
-        public void Clear() => Lines.Clear();
+        public virtual void Clear() => Lines.Clear();
     }
 
     public class CartLine
---
+++ SportsStore/SportsStore/Models/EFOrderRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models
+{
+    public class EFOrderRepository : IOrderRepository
+    {
+        private StoreDbContext _dbContext;
+
+        public EFOrderRepository(StoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IQueryable<Order> Orders => _dbContext.Orders.Include(x => x.Lines).ThenInclude(x => x.Product);
+
+        public void SaveOrder(Order order)
+        {
+            _dbContext.AttachRange(order.Lines.Select(x => x.Product));
+            if (order.OrderId == 0)
+            {
+                _dbContext.Orders.Add(order);
+            }
+
+            _dbContext.SaveChanges();
+        }
+    }
+}
