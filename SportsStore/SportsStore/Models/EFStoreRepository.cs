8ec1a1173a9870e4ef2cc71023a9b805ad6b1df3
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-03-07 17:20:08 +0400

    added administrator space

--- SportsStore/SportsStore/Models/EFStoreRepository.cs
+++ SportsStore/SportsStore/Models/EFStoreRepository.cs
@@ -10,5 +10,22 @@
         }
 
         public IQueryable<Product> Products => _dbContext.Products;
+
+        public void CreateProduct(Product product)
+        {
+            _dbContext.Add(product);
+            _dbContext.SaveChanges();
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            _dbContext.Remove(product);
+            _dbContext.SaveChanges();
+        }
+
+        public void SaveProduct(Product product)
+        {
+            _dbContext.SaveChanges();
+        }
     }
 }
