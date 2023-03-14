11221dbee6ab063ae6ab1830d36147b4d262304d
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-03-14 12:59:14 +0400

    Constraining Segment Matching

--- Platform/Platform/Program.cs
+++ Platform/Platform/Program.cs
@@ -23,7 +23,7 @@ var app = builder.Build();
 
 
 //segment variables in url patterns
-app.MapGet("{first}/{second}/{*catchall}", async context =>
+app.MapGet("{first:alpha:length(3)}/{second:bool}", async context =>
 {
     await context.Response.WriteAsync("Request was Routed \n");
     foreach (var kvp in context.Request.RouteValues)
