02f8bd3f676157932ed315b55145713cf6ea736b
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-03-14 13:31:41 +0400

    Accessing the Endpoint in a Middleware Component

--- Platform/Platform/Program.cs
+++ Platform/Platform/Program.cs
@@ -8,12 +8,27 @@ builder.Services.Configure<RouteOptions>(opts =>
 {
     opts.ConstraintMap.Add("countryName", typeof(CountryRouteConstrait));
 });
+
 //builder.Services.Configure<MessageOptions>(opts =>
 //{
 //    opts.CityName = "Albany";
 //});
 
 var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    Endpoint? end = context.GetEndpoint();
+    if (end != null)
+    {
+        await context.Response.WriteAsync($"{end.DisplayName} Selected \n");
+    }
+    else
+    {
+        await context.Response.WriteAsync("No endpoint Selected \n");
+    }
+    await next();
+});
 
 //app.UseMiddleware<Population>();
 //app.UseMiddleware<Capital>();
@@ -27,22 +42,35 @@ var app = builder.Build();
 
 
 //segment variables in url patterns
-app.MapGet("{first:alpha:length(3)}/{second:bool}", async context =>
-{
-    await context.Response.WriteAsync("Request was Routed \n");
-    foreach (var kvp in context.Request.RouteValues)
-    {
-        await context.Response.WriteAsync($"{kvp.Key}: {kvp.Value}\n");
-    }
-});
+//app.MapGet("{first:alpha:length(3)}/{second:bool}", async context =>
+//{
+//    await context.Response.WriteAsync("Request was Routed \n");
+//    foreach (var kvp in context.Request.RouteValues)
+//    {
+//        await context.Response.WriteAsync($"{kvp.Key}: {kvp.Value}\n");
+//    }
+//});
 
-app.MapGet("capital/{country:regex(^uk|france|monaco$)}", Capital.Endpoint);
-app.MapGet("size/{city?}", Population.Endpoint).WithMetadata(new RouteNameMetadata("population"));
+//app.MapGet("capital/{country:countryName}", Capital.Endpoint);
+
+//app.MapGet("capital/{country:regex(^uk|france|monaco$)}", Capital.Endpoint);
+//app.MapGet("size/{city?}", Population.Endpoint).WithMetadata(new RouteNameMetadata("population"));
+
+app.Map("{number:int}", async context =>
+{
+    await context.Response.WriteAsync("Routed to the int endpoint");
+}).WithDisplayName("Int Endpoint").Add(b => ((RouteEndpointBuilder)b).Order = 1);
+
+app.Map("{number:double}", async context =>
+{
+    await context.Response.WriteAsync("Routed to the double endpoint");
+}).WithDisplayName("Double Endpoint").Add(b => ((RouteEndpointBuilder)b).Order = 2);
 
 app.MapFallback(async context =>
 {
     await context.Response.WriteAsync("Routed to fallback endpoint");
 });
+
 //app.Run(async (context) =>
 //{
 //    await context.Response.WriteAsync("Terminal middleware reached");
