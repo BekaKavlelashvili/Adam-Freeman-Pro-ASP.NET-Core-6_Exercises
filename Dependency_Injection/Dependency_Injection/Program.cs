f87d626bc2b36e84ed64312c04fea0cc0d06060c
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-03-17 17:13:13 +0400

    unbound service

--- Dependency_Injection/Dependency_Injection/Program.cs
+++ Dependency_Injection/Dependency_Injection/Program.cs
@@ -4,6 +4,9 @@ using Dependency_Injection.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 
+
+builder.Services.AddSingleton(typeof(ICollection<>), typeof(List<>));
+
 //IWebHostEnvironment env = builder.Environment;
 
 //if (env.IsDevelopment())
@@ -27,24 +30,30 @@ var builder = WebApplication.CreateBuilder(args);
 
 //builder.Services.AddScoped<ITimeStamping, DefaultTimeStamper>();
 
-builder.Services.AddScoped<IResponseFormatter, TextResponseFormatter>();
-builder.Services.AddScoped<IResponseFormatter, HtmlResponseFormatter>();
-builder.Services.AddScoped<IResponseFormatter, GuidService>();
+//builder.Services.AddScoped<IResponseFormatter, TextResponseFormatter>();
+//builder.Services.AddScoped<IResponseFormatter, HtmlResponseFormatter>();
+//builder.Services.AddScoped<IResponseFormatter, GuidService>();
 
 var app = builder.Build();
 
-
-app.MapGet("single", async context =>
-{
-    IResponseFormatter formatter = context.RequestServices.GetRequiredService<IResponseFormatter>();
-    await formatter.Format(context, "single service");
+app.MapGet("string", async context => {
+    ICollection<string> collection
+    = context.RequestServices.GetRequiredService<ICollection<string>>();
+    collection.Add($"Request: {DateTime.Now.ToLongTimeString()}");
+    foreach (string str in collection)
+    {
+        await context.Response.WriteAsync($"String: {str}\n");
+    }
 });
 
-app.MapGet("/", async context =>
-{
-    IResponseFormatter formatter = context.RequestServices
-    .GetServices<IResponseFormatter>().First(f => f.RichOutput);
-    await formatter.Format(context, "Multiple Services");
+app.MapGet("int", async context => {
+    ICollection<int> collection
+    = context.RequestServices.GetRequiredService<ICollection<int>>();
+    collection.Add(collection.Count() + 1);
+    foreach (int val in collection)
+    {
+        await context.Response.WriteAsync($"Int: {val}\n");
+    }
 });
 
 //app.UseMiddleware<WeatherMiddleware>();
